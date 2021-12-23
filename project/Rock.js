cd3912321d558daf0794d62aead1646389ff6305
GamemasterOF SwordArtOnlineMC <n00201327@iadt.ie>
2021-12-23 20:28:08 +0000

    Prototyping the project in a 2-Dimensional plane

--- project/Rock.js
+++ project/Rock.js
@@ -1,10 +1,49 @@
 class Rock {
-    constructor(x, y) {
-        this.pos = createVector(x, y);
-        this.velocity = random(0, 1);
+    constructor() {
+        this.spawnHeight = [-100, height+100];
+        this.x = random(0, width);
+        this.y = random(this.spawnHeight);
+        this.rotation = this.checkCenter();
+        this.size = random(1, 50);
+        this.vx = random(0, 1);
+        this.vy = random(0, 1);
     }
 
     render() {
-        
+        push();
+        translate(this.x, this.y);
+        rotate(this.rotation);
+        fill(90);
+		beginShape();
+		vertex(10, 0);
+		vertex(-10, 10);
+		vertex(-5, 0);
+		vertex(-10, -10);
+		vertex(10, 0);
+		endShape();
+        pop();
     }
+
+    accelerateRock(ax, ay) {
+        this.vx = this.vx + ax;
+        this.vy = this.vy + ay;
+    }
+
+    rotateRock(rotate) {
+        this.rotation = rotate;
+    }
+
+    checkCenter() {
+        let dx, dy, centerPos;
+        dx = (width / 2) - this.x;
+        dy = (height / 2) - this.y;
+        centerPos = atan2(dy, dx);
+        return centerPos;
+    }
+
+    moveRock()
+	{
+		this.x = this.x + this.vx;
+		this.y = this.y + this.vy;
+	}
 }
--- project/sketch.js
+++ project/sketch.js
@@ -1,23 +1,75 @@
-let sun;
+// let sun;
 
+
+// function setup() {
+//     createCanvas(600, 600, WEBGL);
+//     sun = new Planet(50, 0, 0);
+//     sun.spawnMoons(1, 1);
+//     var easy=createEasyCam();
+//     let state = {
+//         distance : 300,                 // scalar
+//         center   : [sun.v.x, sun.v.y, sun.v.z],         // vector
+//         rotation : [1.000, 0.000, 0.000, 0.000],
+//     };
+//     easy.setState(state, 1000); // animate to state over the period of 1 second
+// }
+
+// function draw() {
+//     background(0);
+//     directionalLight(250, 250, 250, 0, 0, -1);
+//     // translate(width/2, height/2);
+//     sun.render();
+//     sun.orbit();
+// }
+
+let rocks = [];
+let force = 0.1;
 
 function setup() {
-    createCanvas(600, 600, WEBGL);
-    sun = new Planet(50, 0, 0);
-    sun.spawnMoons(1, 1);
+    createCanvas(750, 500, WEBGL);
+    // rock = new Rock();
+
     var easy=createEasyCam();
     let state = {
         distance : 300,                 // scalar
-        center   : [sun.v.x, sun.v.y, sun.v.z],         // vector
+        center   : [width/2,height/2, 0],         // vector
         rotation : [1.000, 0.000, 0.000, 0.000],
     };
     easy.setState(state, 1000); // animate to state over the period of 1 second
 }
 
 function draw() {
-    background(0);
-    directionalLight(250, 250, 250, 0, 0, -1);
-    // translate(width/2, height/2);
-    sun.render();
-    sun.orbit();
+    background(255);
+    push();
+    fill(90);
+    translate(width/2, height/2);
+    sphere(30);
+    pop();
+    // rock.render();
+    // rock.rotateRock(rock.checkCenter());
+    // rock.moveRock();
+    // rock.accelerateRock(cos(rock.rotation) * force, sin(rock.rotation) * force);
+
+    for(let i = rocks.length - 1; i >= 0; i--) {
+        // push();
+        rocks[i].render();
+        rocks[i].rotateRock(rocks[i].checkCenter());
+        rocks[i].moveRock();
+        rocks[i].accelerateRock(cos(rocks[i].rotation) * force, sin(rocks[i].rotation) * force);
+        // if (rocks[i].x >= width + 300 || rocks[i].x <= -300 || rocks[i].y <= -100 || rocks[i].y >= width + 100) {
+        //     rocks.splice(i, 1);
+        // }
+        // pop();
+
+    }
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+        rocks.push(new Rock());
+    }
+}
+
+function loopRock() {
+    
 }
